49b1386552aecef24958374b8f30222ea295004a
jokereactive <sarthak12088@iiitd.ac.in>
2014-07-19 18:59:27 +0530

    Added Individual Question Pop Up

--- gen/com/example/stickyagent/R.java
+++ gen/com/example/stickyagent/R.java
@@ -32,7 +32,8 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int ratingscreen=0x7f030001;
+        public static final int popupdialogue=0x7f030001;
+        public static final int ratingscreen=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
